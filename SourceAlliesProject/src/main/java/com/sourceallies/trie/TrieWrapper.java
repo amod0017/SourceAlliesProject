6d9970ac2c08e4d4462c8c0ed7fc155918e83737
amod0017 <amod0017@gmail.com>
2017-08-10 03:04:48 -0400

    TrieWrapper: A wrapper class which maintains the most frequent element at any given point of time

---
+++ SourceAlliesProject/src/main/java/com/sourceallies/trie/TrieWrapper.java
@@ -0,0 +1,55 @@
+package com.sourceallies.trie;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This class maintains a list of most frequenct word inserted at any given
+ * instance
+ * 
+ * @author amod0
+ *
+ */
+public class TrieWrapper {
+	private static final int MAX = 3;
+	private final Trie root;
+	private final List<MostFrequentWordsWithFreq> mostFreqWords;
+
+	TrieWrapper() {
+		root = new Trie();
+		mostFreqWords = new ArrayList<>();
+	}
+
+	public List<MostFrequentWordsWithFreq> getMostFreqWords() {
+		return mostFreqWords;
+	}
+
+	public void trieInsert(final String word) {
+		root.insert(word);
+		final int indexOf = mostFreqWords.indexOf(new MostFrequentWordsWithFreq(word, root.getCount(word) - 1));
+		if (mostFreqWords.size() < MAX) {
+			// if mostFreq contains element
+			if (indexOf > -1) {
+				mostFreqWords.get(indexOf).increaseFrequency();
+			} else {
+				mostFreqWords.add(new MostFrequentWordsWithFreq(word, root.getCount(word)));
+			}
+		} else {
+			// element is in list so increase freq
+			if (indexOf != -1) {
+				mostFreqWords.get(indexOf).increaseFrequency();
+			}
+			// if word.freq is greater than the minimum
+			// remove minimum element i.e. first
+			// add new element to list
+			// sort list
+			else if (root.getCount(word) >= mostFreqWords.get(0).getFreq()) {
+				mostFreqWords.remove(0);
+				mostFreqWords.add(new MostFrequentWordsWithFreq(word, root.getCount(word)));
+			} // else it's fine no update needed in list
+		}
+		Collections.sort(mostFreqWords);
+	}
+
+}
